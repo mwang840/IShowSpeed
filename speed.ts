a9be78943df0f6f377312f612ece442c14b9498c
Maxwell Wang <maxwang@udel.edu>
2022-06-19 12:08:38 -0400

    client

--- speed.ts
+++ speed.ts
@@ -1,3 +1,10 @@
-import DiscordJs from 'discord.js'
+import DiscordJs, { Intents } from 'discord.js'
 import dotenv from 'dotenv'
 dotenv.config()
+
+const client = new DiscordJs.Client({
+    intents:[
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES
+    ]
+})

8e3c2ed72064f055da7bcaa16e861cc3f97793bc
Maxwell Wang <maxwang@udel.edu>
2022-07-23 10:23:45 -0400

    set welcome channel

--- commands/setWelcomeToSpeed.ts
+++ commands/setWelcomeToSpeed.ts
@@ -1,5 +1,6 @@
 import {ICommand} from "wokcommands";
 import DJS from "discord.js";
+import welcomeToSpeed from "./welcomeToSpeed";
 
 export default{
     category: "configuration",
@@ -34,5 +35,23 @@ export default{
         if(!targeted || targeted.type == 'GUILD_TEXT'){
             return 'Specify channel please'
         }
+
+        let response = interaction?.options.getString('text')
+
+        if(response){
+            args.shift()
+            response = args.join('')
+        }
+
+        await welcomeToSpeed.findOneAndUpdate({
+            _id: guild.id
+        }, {
+            _id: guild.id,
+            response,
+            channelId: targeted.id
+        },{
+            upsert: true
+        })
+        return 'Welcome channel set!'
     }
 } as ICommand
